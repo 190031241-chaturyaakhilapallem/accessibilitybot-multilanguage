8959ecb2f16d5ad6f2f04aade09f2e4cccc8e9b3
Chaturya Akhila Pallem <84761729+190031241-chaturyaakhilapallem@users.noreply.github.com>
2024-11-08 21:44:28 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Main from './components/Main/Main'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
 
 
   return (
     <>
+      <Sidebar/>
       <Main/>  
     </>
   )
